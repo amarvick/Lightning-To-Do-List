66d2473e1e1790fb72a75e26672b4abb4698991d
Alex <alex@fiduciarytech.com>
2018-09-06 14:11:55 -0700

    Removed 'MarvickApp' components

--- src/aura/MarvickApplication/MarvickApplication.app
+++
@@ -1,3 +0,0 @@
-<aura:application extends="force:slds">
-    <!--<c:MarvickAppHeader />-->
-</aura:application>
--- src/aura/MarvickApplicationComponent/MarvickApplicationComponentController.js
+++
@@ -1,6 +0,0 @@
-({
-	marvify : function(component, event, helper) {
-        const message = component.get('v.message');
-        console.log(message);
-	}
-})
